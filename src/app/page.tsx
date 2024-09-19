056f88d1c2da5abc2b1988d6ae2207f9691948ea
ChadPeterSmith <chad.smith@igrid.co.za>
2024-09-19 18:48:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,6 @@ import Image from "next/image"
 export default function Home() {
   return (
    <main>
-    <Image
-     src="/nextjs-github-pages/vercel.svg"
-     alt="Vercel Logo"
-     width={100}
-     height={24}
-     priority
-   />
     <HomePage />
    </main>
   );
